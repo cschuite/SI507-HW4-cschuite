9991514ae542f399f490d6f5c856e7fae49b8246
ooudsema <35814400+ooudsema@users.noreply.github.com>
2018-01-29 16:04:08 -0500

    Update magic_eight.py

--- magic_eight.py
+++ magic_eight.py
@@ -4,7 +4,7 @@ def ask_question():
 	response = ""
 	while response != "quit":
 		response = input("What is your question? Enter 'quit' to quit.")
-		if response[-1] != "?" and response != "quit":
+		while response[-1] != "?" and response != "quit":
 			print("I'm sorry, I can only answer questions.")
 			response = input("Please input a question this time.")
 		answers = ["It is certain", "It is decidedly so", "Without a doubt", "Yes definitely", "You may rely on it", "As I see it, yes", "Most likely", "Outlook good", "Yes", "Signs point to yes", "Reply hazy try again", "Ask again later", "Better not tell you now", "Cannot predict now", "Concentrate and ask again", "Don't count on it", "My reply is no", "My sources say no", "Outlook not so good", "Very doubtful"]
