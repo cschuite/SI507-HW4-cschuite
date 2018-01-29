00609e30b1b262f77eeb68afc83b5bae5aca50b3
ooudsema <35814400+ooudsema@users.noreply.github.com>
2018-01-29 15:49:49 -0500

    Update magic_eight.py

--- magic_eight.py
+++ magic_eight.py
@@ -7,12 +7,10 @@ def ask_question():
 			print("I'm sorry, I can only answer questions.")
 			response = input("Please input a question this time.")
 		response = input("What is your question? Enter 'quit' to quit.")
-	return response
+		answers = ["It is certain", "It is decidedly so", "Without a doubt", "Yes definitely", "You may rely on it", "As I see it, yes", "Most likely", "Outlook good", "Yes", "Signs point to yes", "Reply hazy try again", "Ask again later", "Better not tell you now", "Cannot predict now", "Concentrate and ask again", "Don't count on it", "My reply is no", "My sources say no", "Outlook not so good", "Very doubtful"]
+		print(answers[random.randrange(21)])
+print(ask_question())
 
-def answer_question():
-	answers = ["It is certain", "It is decidedly so", "Without a doubt", "Yes definitely", "You may rely on it", "As I see it, yes", "Most likely", "Outlook good", "Yes", "Signs point to yes", "Reply hazy try again", "Ask again later", "Better not tell you now", "Cannot predict now", "Concentrate and ask again", "Don't count on it", "My reply is no", "My sources say no", "Outlook not so good", "Very doubtful"]
-	return answers[random.randrange(21)]
-print(answer_question())
 
 
 
