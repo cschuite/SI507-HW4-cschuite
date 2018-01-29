53489c708d8aeee9c6ce7a8051f8e11534e795b7
cschuite <35494702+cschuite@users.noreply.github.com>
2018-01-29 15:35:04 -0500

    edits to check_question branch

--- magic_eight.py
+++ magic_eight.py
@@ -1,7 +1,9 @@
 def ask_question():
-	response = input("What is your question?")
+	response = input("What is your question? Enter 'quit' to quit.")
+	while response != "quit":
+		if response[-1] != "?":
+			print("I'm sorry, I can only answer questions.")
+			response = input("Please input a question this time.")
+		response = input("What is your question? Enter 'quit' to quit.")
 	return response
-
-print(ask_question())
-
 
