725e3b3c96f2bf8741d874c84a88f625fec669c9
cschuite <35494702+cschuite@users.noreply.github.com>
2018-01-29 17:10:44 -0500

    Update magic_eight.py

--- magic_eight.py
+++ magic_eight.py
@@ -4,12 +4,13 @@ def ask_question():
 	response = ""
 	while response != "quit":
 		response = input("What is your question? Enter 'quit' to quit.")
-		while response[-1] != "?" and response != "quit":
+		if response = "quit":
+			return None
+		while response[-1] != "?":
 			print("I'm sorry, I can only answer questions.")
 			response = input("Please input a question this time.")
-		while response != "quit":
-			answers = ["It is certain", "It is decidedly so", "Without a doubt", "Yes definitely", "You may rely on it", "As I see it, yes", "Most likely", "Outlook good", "Yes", "Signs point to yes", "Reply hazy try again", "Ask again later", "Better not tell you now", "Cannot predict now", "Concentrate and ask again", "Don't count on it", "My reply is no", "My sources say no", "Outlook not so good", "Very doubtful"]
-			print(answers[random.randrange(20)])
+		answers = ["It is certain", "It is decidedly so", "Without a doubt", "Yes definitely", "You may rely on it", "As I see it, yes", "Most likely", "Outlook good", "Yes", "Signs point to yes", "Reply hazy try again", "Ask again later", "Better not tell you now", "Cannot predict now", "Concentrate and ask again", "Don't count on it", "My reply is no", "My sources say no", "Outlook not so good", "Very doubtful"]
+		print(answers[random.randrange(20)])
 	return ""
 print(ask_question())
 
